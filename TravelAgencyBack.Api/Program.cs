2b7267b4c6bf29dca0cab9e71a462dd968f0e0ab
bdmtnz <martinez_o@hotmail.es>
2023-12-23 02:15:35 -0500

    .

--- TravelAgencyBack.Api/Program.cs
+++ TravelAgencyBack.Api/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<TravelAgencyContext>(opt =>
 {
-    opt.UseInMemoryDatabase("TravelAgencyDbInMemory");
+    opt.UseInMemoryDatabase(Guid.NewGuid().ToString());
 });
 
 //Dependency inversion
