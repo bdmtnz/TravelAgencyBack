aa73d917f946c92262b5fec721e360ada530ac67
bdmtnz <martinez_o@hotmail.es>
2023-12-25 14:56:52 -0500

    End filter booking handler

--- TravelagencyBack.Application/BookingHandler/Filter/FilterBookingQuery.cs
+++ TravelagencyBack.Application/BookingHandler/Filter/FilterBookingQuery.cs
@@ -1,12 +1,51 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TravelAgencyBack.Application.Base;
+using TravelAgencyBack.Domain;
+using TravelAgencyBack.Domain.Contracts;
 
 namespace TravelAgencyBack.Application.BookingHandler.Filter
 {
-    public class FilterBookingQuery
+    public class FilterBookingQuery : IRequestHandler<FilterBookingRequest, ApiResponse<IEnumerable<Booking>>>
     {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IGenericRepository<Booking> _bookingRepository;
+
+        public FilterBookingQuery(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            _bookingRepository = unitOfWork.GenericRepository<Booking>();
+        }
+
+        public Task<ApiResponse<IEnumerable<Booking>>> Handle(FilterBookingRequest request, CancellationToken cancellationToken)
+        {
+            ApiResponse<IEnumerable<Booking>> response;
+            IEnumerable<Booking> bookings;
+
+            Func<Booking, bool> filter = (booking) =>
+            {
+                return
+                    string.IsNullOrEmpty(request.HotelId) ? true : booking.Room.Hotel.Id == request.HotelId &&
+                    string.IsNullOrEmpty(request.RoomId) ? true : booking.Room.Id == request.RoomId &&
+                    string.IsNullOrEmpty(request.TravelerId) ? true : booking.Traveler.Id == request.TravelerId &&
+                    request.Start is null ? true : (booking.Start - request.Start.Value).Days == 0 &&
+                    request.End is null ? true : (booking.End - request.End.Value).Days == 0 &&
+                    request.Enabled is null ? true : booking.Enabled == request.Enabled;
+            };
+
+            bookings = _bookingRepository.FindBy(filter);
+
+            response = new ApiResponse<IEnumerable<Booking>>()
+            {
+                Status = System.Net.HttpStatusCode.OK,
+                Message = Resources.OkResponseES.SUCCESSFUL_PROCCESS,
+                Data = bookings
+            };
+            return Task.FromResult(response);
+        }
     }
 }
--- TravelagencyBack.Application/BookingHandler/Filter/FilterBookingRequest.cs
+++ TravelagencyBack.Application/BookingHandler/Filter/FilterBookingRequest.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TravelAgencyBack.Application.Base;
+using TravelAgencyBack.Domain;
 
 namespace TravelAgencyBack.Application.BookingHandler.Filter
 {
-    public class FilterBookingRequest
+    public class FilterBookingRequest : IRequest<ApiResponse<IEnumerable<Booking>>>
     {
+        public string? HotelId { get; set; }
+        public string? RoomId { get; set; }
+        public string? TravelerId { get; set; }
+        public DateTime? Start { get; set; }
+        public DateTime? End { get; set; }
+        public bool? Enabled { get; set; }
     }
 }
